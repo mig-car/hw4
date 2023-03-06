7704e1e222d0d435ead1e7a968e9c51632a40123
mig-car <miguel.caruncho@kellogg.northwestern.edu>
2023-03-06 20:38:59 +0000

    signup OK

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   before_action :current_user
 
   def current_user
-    puts "------------------ code before every request ------------------"
-  end
+    # find current user from user_id of initialized session
+    @current_user = User.find_by({ "id" => session["user_id"] })
+ end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,8 +5,12 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new
-    @user["first_name"] = params["user"]["first_name"]
-    @user["last_name"] = params["user"]["last_name"]
+
+    # Removing first_name and last_name as it's not required and not part of schema.
+    # @user["first_name"] = params["user"]["first_name"]
+    # @user["last_name"] = params["user"]["last_name"]
+    
+    @user["username"] = params["user"]["username"]
     @user["email"] = params["user"]["email"]
     @user["password"] = params["user"]["password"]
     @user.save
