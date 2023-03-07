cd171527f758e8a43fd0ea05b627a902ff1404e8
mig-car <miguel.caruncho@kellogg.northwestern.edu>
2023-03-07 04:17:24 +0000

    added optional image + the css bootstrap styling

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,6 +12,7 @@ class PostsController < ApplicationController
     @post["posted_on"] = params["post"]["posted_on"]
     @post["place_id"] = params["post"]["place_id"]
     @post["user_id"] = session["user_id"]
+    @post["image"] = params["post"]["image"]
     @post.save
     redirect_to "/places/#{@post["place_id"]}"
   end
--- db/schema.rb
+++ db/schema.rb
@@ -23,6 +23,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_03_09_020701) do
     t.date "posted_on"
     t.integer "place_id"
     t.integer "user_id"
+    t.string "image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
