1ebbf40b47c4f54acc5a97d20419e38b36c7adce
mig-car <miguel.caruncho@kellogg.northwestern.edu>
2023-03-11 17:34:32 +0000

    Added navbar, cleaned up margins for buttons

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,7 +11,8 @@ class PostsController < ApplicationController
     @post["description"] = params["post"]["description"]
     @post["posted_on"] = params["post"]["posted_on"]
     @post["place_id"] = params["post"]["place_id"]
-    @post["user_id"] = session["user_id"]
+    @post["user_id"] = @current_user["id"]
+    #session["user_id"]
     @post["image"] = params["post"]["image"]
     @post.save
     redirect_to "/places/#{@post["place_id"]}"
